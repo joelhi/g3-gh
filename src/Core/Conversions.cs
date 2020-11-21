b9bd33fb8fddc22db4ef82e377a93ec91c93c888
joelhi <d.j.hilmersson@gmail.com>
2020-11-21 16:30:17 +0000

    Started building conversions and base gh-goo

---
+++ src/Core/Conversions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using g3;
+
+namespace gh3sharp.Core
+{
+    public static class Conversions
+    {
+        public static DMesh3 ToDMesh3(this Rhino.Geometry.Mesh ms)
+        {
+
+            int numV = ms.Vertices.Count;
+            int numF = ms.Faces.Count;
+
+            List<Vector3f> Vertices = new List<Vector3f>(numV);
+            List<Vector3i> Triangles = new List<Vector3i>(numF);
+
+            for (int i = 0; i < numV; i++)
+                Vertices.Add(ms.Vertices[i].ToVec3f());
+
+            for (int i = 0; i < numF; i++)
+                Triangles.Add(ms.Faces[i].ToVec3i());
+
+            return DMesh3Builder.Build<Vector3f,Vector3i,Vector3f>(Vertices, Triangles);
+        }
+
+        public static Rhino.Geometry.Mesh ToRhino(this DMesh3 dMesh3)
+        {
+            Rhino.Geometry.Mesh rhMs = new Rhino.Geometry.Mesh();
+
+            List<Rhino.Geometry.MeshFace> rhFaces = new List<Rhino.Geometry.MeshFace>();
+
+            foreach (var tri in dMesh3.Triangles())
+            {
+
+            }
+
+            
+        }
+        public static Vector3f ToVec3f(this Rhino.Geometry.Point3f rhPt)
+        {
+            return new Vector3f(rhPt.X,rhPt.Y,rhPt.Z);
+        }
+
+        public static Vector3i ToVec3i(this Rhino.Geometry.MeshFace rhMeshFace)
+        {
+            if (rhMeshFace.IsTriangle)
+                return new Vector3i(rhMeshFace.A, rhMeshFace.B, rhMeshFace.C);
+            else
+                throw new Exception("The mesh face needs to be a triangle.");
+                    
+        }
+    }
+}
---
+++ src/Core/Goos/DMesh3_goo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace gh3sharp.Core.Goos
+{
+    public class DMesh3_goo
+    {
+        public DMesh3_goo()
+        {
+        }
+    }
+}
